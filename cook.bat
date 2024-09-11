c980144054cef2336249a00193e9d6f655cc3c17
Ongun Korkmaz <53625744+Ongun1905@users.noreply.github.com>
2024-09-11 19:28:53 +0300

    Delete cook.bat

--- cook.bat
+++
@@ -1,3 +0,0 @@
-cd /d "Engine\Build\BatchFiles"
-RunUAT.bat BuildCookRun -project=Z:\ProjectName\Game\ProjectName\ProjectName.uproject -platform=Win64 -configuration=Development -build -cook -iterate -pak -stage -client -server
-cd ..\..\..
